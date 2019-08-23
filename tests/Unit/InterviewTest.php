c30f9b235504c0a04afb970b871a56f064e4a923
jhkiehna <jhkiehna@gmail.com>
2019-08-23 15:27:57 -0400

    fixed class name for InterviewTest

--- tests/Unit/InterviewTest.php
+++ tests/Unit/InterviewTest.php
@@ -6,7 +6,7 @@ use App\Interview;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
-class UserTest extends TestCase
+class InterviewTest extends TestCase
 {
     use RefreshDatabase;
 
