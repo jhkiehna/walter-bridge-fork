7c5315e5872170945ec8b855b24aa8e5537db4f5
jhkiehna <jhkiehna@gmail.com>
2019-08-23 16:35:08 -0400

    wrote another throwaway test to ensure relationships work as I expect

--- tests/Unit/UserTest.php
+++ tests/Unit/UserTest.php
@@ -3,7 +3,10 @@
 namespace Tests\Unit;
 
 use App\User;
+use App\Sendout;
+use App\Interview;
 use Tests\TestCase;
+use App\CandidateCoded;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class UserTest extends TestCase
@@ -14,7 +17,20 @@ class UserTest extends TestCase
     {
         $user = factory(User::class)->create();
 
+        factory(CandidateCoded::class)->create([
+            'central_id' => $user->central_id
+        ]);
+        factory(Interview::class)->create([
+            'central_id' => $user->central_id
+        ]);
+        factory(Sendout::class)->create([
+            'central_id' => $user->central_id
+        ]);
+
         $this->assertTrue($user->walter_id != null);
         $this->assertTrue($user->central_id != null);
+        $this->assertTrue($user->candidatesCoded()->first() != null);
+        $this->assertTrue($user->interviews()->first() != null);
+        $this->assertTrue($user->sendouts()->first() != null);
     }
 }
