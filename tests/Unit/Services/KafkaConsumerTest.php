1615b74fa91ee696272e0379d455df54d0499180
Ethan Knowlton <eknowlton@gmail.com>
2019-09-06 16:11:03 -0400

    cleaned up the kafka tests

--- tests/Unit/Services/KafkaConsumerTest.php
+++ tests/Unit/Services/KafkaConsumerTest.php
@@ -2,12 +2,8 @@
 
 namespace Tests\Unit\Services;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
-
 class KafkaConsumerTest extends \Tests\TestCase
 {
-    //use RefreshDatabase;
-
     public function testItCanConnectToKafka()
     {
         $this->markTestSkipped("This isn't really needed unless nmred/kafka-php changes.");
--- tests/Unit/Services/KafkaProducerTest.php
+++ tests/Unit/Services/KafkaProducerTest.php
@@ -2,12 +2,8 @@
 
 namespace Tests\Unit\Services;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
-
 class KafkaProducerTest extends \Tests\TestCase
 {
-    //use RefreshDatabase;
-
     public function testItCanConnectToKafka()
     {
         $this->markTestSkipped("This isn't really needed unless nmred/kafka-php changes.");
