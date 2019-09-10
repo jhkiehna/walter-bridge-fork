023d57465502495a49be813d3bbe32b0f97749f4
Ethan Knowlton <eknowlton@gmail.com>
2019-09-10 14:45:43 -0400

    some docblock for publish

--- app/Services/KafkaProducer.php
+++ app/Services/KafkaProducer.php
@@ -13,6 +13,16 @@ class KafkaProducer
         $this->producer = $producer;
     }
 
+    /**
+     * Publish a message to the event stream.
+     *
+     * Be sure to `json_encode` the message before passing the
+     * message.
+     *
+     * @param string $topic The topic to publish the message to.
+     * @param string $message JSON encoded object string.
+     * @param string $key This isn't used right now.
+     */
     public function publish($topic, $message, $key = '')
     {
         return $this->producer->send(
